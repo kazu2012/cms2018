4d814bf21bea7d8f6a8b0d4e8098cec6513dccb9
kazu2012 <cat_shou@yahoo.co.jp>
2019-05-08 19:05:26 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ var config = {
   // and branch to get files from.
   github: {
     username: 'kazu2012',
-    repo: 'cms.js-starter',
+    repo: 'cms2018',
     branch: 'master',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
